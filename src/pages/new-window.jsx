77a74907f8fc7063ab72ce24af567de066c56e2b
Stuart Hull <stuart.hull@autotrader.co.uk>
2022-11-01 09:29:07 +0000

    fourth push of pages

--- src/pages/new-window.jsx
+++ src/pages/new-window.jsx
@@ -4,10 +4,11 @@ function NewWindow() {
     return (
         <Fragment>
             <h1 aria-hidden="true">New window warning</h1>
-            <p>This is an <a className="info"
-                             href="index.html" target="_blank">
-                external link <span className="visuallyhidden">This opens a new
-                window</span></a>
+            <p>This is an
+                <a className="info" href="index.html" target="_blank">
+                    external link
+                    <span className="visuallyhidden">This opens a new window</span>
+                </a>
             </p>
         </Fragment>
     );
